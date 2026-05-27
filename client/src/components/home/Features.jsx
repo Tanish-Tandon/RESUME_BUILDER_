670c26ce6411ec145e1bbb0b52786c816831b4ad
Tanish-Tandon <tandontanish99@gmail.com>
2026-05-27 13:16:03 +0530

    ADD TESTIMONIAL COMPONENT

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -1,5 +1,6 @@
-import { Zap, ZapIcon } from 'lucide-react';
+import { Zap } from 'lucide-react';
 import React from 'react'
+import Title from './Title';
 
 const Features = () => {
 
@@ -21,7 +22,12 @@ const Features = () => {
 
             <span>Simple process  </span>
         </div>
+
+
+
+         <Title title='Build your resume' description='Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features.'/>
  
+
 
         
 
@@ -35,7 +41,7 @@ const Features = () => {
 
 
 
-            <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="flex flex-col md:flex-row items-center xl:-mt-10">
                 <img className="max-w-2xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
                 <div className="px-4 md:px-0" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
                     <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
