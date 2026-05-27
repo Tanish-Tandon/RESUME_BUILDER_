dce0e20ab15291d319c68251c389eb7db714153d
Tanish-Tandon <tandontanish99@gmail.com>
2026-05-27 21:25:22 +0530

    ADD create resume functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FilePenLineIcon, PlusIcon,UploadCloudIcon,TrashIcon,PencilIcon } from 'lucide-react'
+import { FilePenLineIcon, PlusIcon,UploadCloudIcon,TrashIcon,PencilIcon,XIcon } from 'lucide-react'
 import React, { useState,useEffect } from 'react'
 import { dummyResumeData } from '../assets/assets'
 import { useNavigate} from 'react-router-dom'
@@ -10,10 +10,29 @@ const Dashboard = () => {
   const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"]
 
   const [allResumes,setAllResumes]=useState([])
+  const [showCreateResume,setShowCreateResume]=useState(false)
+  const[showUploadResume,setShowUploadResume]=useState(false)
+  const [title,setTitle]=useState('')
+  const [resume,setResume]=useState(null)
+  const [editResumeId,setEditResumeId]=useState('')
+
+  
+
+
+
+
 
   const loadAllResumes=async()=>{
     setAllResumes(dummyResumeData)  
   } 
+
+  const createResume=async(event)=>{ 
+    event.preventDefault()
+    setShowCreateResume(false)
+    navigate(`/app/builder/res123`)
+
+  }
+
 
   useEffect(()=>{
     loadAllResumes()
@@ -36,7 +55,7 @@ const Dashboard = () => {
 
 
 
-          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+          <button onClick={()=>setShowCreateResume(true)} className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
 
                           <PlusIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500  text-white rounded-full'/>
          
@@ -136,6 +155,24 @@ const Dashboard = () => {
                   
               </div>
 
+
+             
+        {showCreateResume && (
+          <form onSubmit={createResume} onClick={()=>setShowCreateResume(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
+           
+            <div onClick={e =>e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+
+
+
+              <h2 className='text-xl font-bold mb-4'>Create a Resume</h2>
+              <input type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required/>
+
+              <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>Create Resume</button>
+              <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=> {setShowCreateResume(false);setTitle('') }}/>
+            </div>
+          </form>
+
+        )}
 
 
  
