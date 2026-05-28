81c01e5f14bcfcca3352d1cb7aaadbee26153000
Tanish-Tandon <tandontanish99@gmail.com>
2026-05-28 18:49:56 +0530

    ADD DELETE AND EDIT FUNCTIONALITY IN OUR PROJECT DASHBOARD

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -40,6 +40,20 @@ const Dashboard = () => {
     navigate(`/app/builder/res123`)
   }
 
+  const editTitle=async(event)=>{
+    event.preventDefault()
+   
+  }
+
+  const deleteResume=async(resumeId)=>{
+    const confirm=window.confirm('Are you sure you want to delete this resume?')
+    if(confirm){
+     setAllResumes(prev=>prev.filter(resume=>resume._id !== resumeId))
+    }
+  }
+
+
+
 
   useEffect(()=>{
     loadAllResumes()
@@ -152,8 +166,8 @@ const Dashboard = () => {
                 
 
        <div onClick={e=> e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                <TrashIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
-                <PencilIcon  className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
+                <TrashIcon onClick={()=>deleteResume(resume._id)} className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
+                <PencilIcon onClick={()=>{setEditResumeId(resume._id);setTitle(resume.title)}}  className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors"/>
               </div>
             </button>
           )
@@ -237,6 +251,26 @@ const Dashboard = () => {
 
           )
         }
+
+
+
+
+          {editResumeId && (
+          <form onSubmit={editTitle} onClick={()=>setEditResumeId('')} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
+           
+            <div onClick={e =>e.stopPropagation()} className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'>
+
+
+
+              <h2 className='text-xl font-bold mb-4'>Edit Resume Title</h2>
+                <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600' required/>
+
+              <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>Update</button>
+              <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=> {setEditResumeId('');setTitle('') }}/>
+            </div>
+          </form>
+
+        )}
 
 
  
