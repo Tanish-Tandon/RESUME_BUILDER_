6d0b253a3f229d4e90722f829450673c17ae0af2
Tanish-Tandon <tandontanish99@gmail.com>
2026-05-30 01:14:37 +0530

    ADD PREVIEW AND TEMPLATES

--- client/src/assets/templates/ModernTemplate.jsx
+++ client/src/assets/templates/ModernTemplate.jsx
@@ -1,4 +1,4 @@
-import { Mail, Phone, MapPin, Globe } from "lucide-react";
+import { Mail, Phone, MapPin,  Globe } from "lucide-react";
 
 const ModernTemplate = ({ data, accentColor }) => {
 	const formatDate = (dateStr) => {
