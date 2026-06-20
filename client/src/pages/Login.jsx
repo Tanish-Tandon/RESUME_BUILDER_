39fa799fe0b33c0d6d521da5e673d552fed1e399
Tanish-Tandon <tandontanish99@gmail.com>
2026-06-20 23:56:33 +0530

    feat: upload,delete,login,logout functionality add

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import { Mail, User2Icon, Lock } from 'lucide-react'
+import { Lock, Mail, User2Icon } from 'lucide-react'
 import React from 'react'
+import api from '../configs/api'
+import { useDispatch } from 'react-redux'
+import { login } from '../app/features/authSlice'
+import toast from 'react-hot-toast'
 
 const Login = () => {
+
+      const dispatch = useDispatch()
 
     const query=new URLSearchParams(window.location.search)
     const urlState=query.get('state')
@@ -14,10 +20,20 @@ const Login = () => {
         password: ''
     })
 
-    const handleSubmit = async (e) => {
+   
+      const handleSubmit = async (e) => {
         e.preventDefault()
-    }
+        try {
+            const { data } = await api.post(`/api/users/${state}`, formData)
+            dispatch(login(data))
+            localStorage.setItem('token', data.token)
+            toast.success(data.message)
+        } catch (error) {
+            toast(error?.response?.data?.message || error.message)
+        }
+    }  
 
+    
  
     const handleChange = (e) => {
         const { name, value } = e.target
