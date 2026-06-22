8e6e9ab4eaadd0926d60df9e61972e39e1a94a67
Tanish-Tandon <tandontanish99@gmail.com>
2026-06-22 23:09:16 +0530

    Update API URL for production

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,8 +1,20 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:9000",
+//     timeout: 10000,
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:9000",
-    timeout: 10000,
+  baseURL:
+    import.meta.env.VITE_BASE_URL ||
+    "https://resume-builder-backend-oh6y.onrender.com",
+  timeout: 10000,
 });
 
 export default api;
